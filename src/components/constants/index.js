d4b5a1a7839b1b133d3877d5f7a8dd4974cb776b
ACER <azat010889@gmail.com>
2022-07-25 17:37:38 +0600

    feat: add PopularPost js

--- src/components/constants/index.js
+++ src/components/constants/index.js
@@ -1,1 +1,8 @@
 export const tags = ['Travel', 'New York', 'London', 'IKEA', 'NORWAY', 'DIY', 'Ideas', 'Baby', 'Family', 'News', 'Clothing', 'Shopping', 'Sports','Games']
+
+export const popularPosts = [
+    {img:'https://www.w3schools.com/w3images/workshop.jpg',title:'Lorem',text:'Sed mattis nunc'},
+    {img:'https://www.w3schools.com/w3images/gondol.jpg',title:'Ipsum',text:'Praes tinci sed'},
+    {img:'https://www.w3schools.com/w3images/skies.jpg',title:'Dorum',text:'Ultricies congue'},
+    {img:'https://www.w3schools.com/w3images/rock.jpg',title:'Mingsum',text:'Lorem ipsum dipsum'}
+]
