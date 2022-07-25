019583c647d2244b21b3877d52b3dd6ce1765142
ACER <azat010889@gmail.com>
2022-07-26 00:25:19 +0600

    feat: add

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header from "./components/Header";
 import BlogEntries from "./components/BlogEntries";
 import IntroductionMenu from "./components/IntroductionMenu";
 import Footer from "./components/Footer";
+import ComponentA from "./components/componentA/ComponentA";
+import ComponentB from "./components/componentB/ComponentB";
 
 
 function App() {
@@ -14,12 +16,14 @@ function App() {
               <div className="w3-row">
                  <BlogEntries/>
                  <IntroductionMenu/>
-
+                 <ComponentA/>
+                 <ComponentB/>
               </div>
           </div>
               <Footer/>
       </>
   );
 }
+
 
 export default App;
--- src/components/Tags.js
+++ src/components/Tags.js
@@ -5,23 +5,26 @@ const margin = {
     marginRight:"5px"
 }
 
-
+const Tag = (props) =>{
+    return(
+        <span
+            className="w3-tag w3-light-grey w3-small w3-margin-bottom"
+            style={margin}>
+            {props.name}
+        </span>
+    )
+}
 
 const Tags = () => {
 
-    const tagsArray = tags.map(item =>(
-        <span
-            className="w3-tag w3-light-grey w3-small w3-margin-bottom"
-            style={margin}>{item}
-        </span>)
-    );
+    const tagsArray = tags.map(item =><Tag name={item}/>);
 
 
     return(
         <>
             <div className="w3-card w3-margin">
                 <div className="w3-container w3-padding">
-                    <h4>Tags</h4>
+                    <h4 className="text-red">Tags</h4>
                 </div>
                 <div className="w3-container w3-white">
                     <p>
