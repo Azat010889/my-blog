32f0f85fe937420f23ac4bbfd5e08f680506fef6
ACER <azat010889@gmail.com>
2022-07-29 10:08:52 +0600

    init: add setActive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 import styles from './Header.module.css'
 
 const Header = () => {
+
+    const setActive = ({isActive}) =>  isActive ? "active" : "";
+
     return (
         <div className={styles.header}>
             <ul>
-                <li><Link to="/">Главная</Link></li>
-                <li><Link to="/about">О нас</Link></li>
-                <li><Link to="/contacts">Контакты</Link></li>
-                <li><Link to="/products">Продукты</Link></li>
-                <li><Link to="/catalog">Каталог</Link></li>
-                <li><Link to="/catalog/iphone">Телефоны</Link></li>
-                <li><Link to="/catalog/macBook">Компьютеры</Link></li>
+                <li><NavLink className={setActive} to="/">Главная</NavLink></li>
+                <li><NavLink className={setActive} to="/about">О нас</NavLink></li>
+                <li><NavLink className={setActive} to="/contacts">Контакты</NavLink></li>
+                <li><NavLink className={setActive} to="/products">Продукты</NavLink></li>
+                <li><NavLink className={setActive} to="/catalog">Каталог</NavLink></li>
+                <li><NavLink className={setActive} to="/catalog/iphone">Телефоны</NavLink></li>
+                <li><NavLink className={setActive} to="/catalog/macBook">Компьютеры</NavLink></li>
 
             </ul>
         </div>
